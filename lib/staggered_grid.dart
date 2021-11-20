2da18c772ed7be38b71c5dad077bb28e35895dd6
MenuItem207 <70802541+MenuItem207@users.noreply.github.com>
2021-11-20 18:40:02 +0800

    Improved staggered grid

--- lib/staggered_grid.dart
+++ lib/staggered_grid.dart
@@ -1,68 +1,85 @@
 library staggeredgrid;
 
 import 'package:flutter/material.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 
 part 'row_builder.dart';
 
 // widget for staggeredGrid
-// must be placed within a widgetwith size just like a list
-class StaggeredGrid extends StatelessWidget {
+class StaggeredGrid extends StatefulWidget {
   final int nOfColumns;
   final IndexedWidgetBuilder itemBuilder;
   final int itemCount;
   final ScrollPhysics? physics;
-  final BoxConstraints minSize; // size of grid
-  final EdgeInsets padding;
+  final bool shrinkWrap;
   StaggeredGrid({
     required this.nOfColumns,
     required this.itemBuilder,
     required this.itemCount,
     this.physics,
-    required this.minSize,
-    this.padding = EdgeInsets.zero,
+    this.shrinkWrap = false,
   });
+
+  @override
+  State<StaggeredGrid> createState() => _StaggeredGridState();
+}
+
+class _StaggeredGridState extends State<StaggeredGrid> {
+  List<ScrollController> controllers = [];
+  late LinkedScrollControllerGroup linkedController;
+
+  @override
+  void initState() {
+    super.initState();
+    linkedController = LinkedScrollControllerGroup();
+    for (int i = 0; i < widget.itemCount; i++) {
+      controllers.add((linkedController.addAndGet()));
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controllers.forEach((controller) => controller.dispose());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: padding,
-        physics: physics ??
-            BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-        child: Container(
-          constraints: BoxConstraints(
-              minHeight: minSize.maxHeight, minWidth: minSize.maxWidth),
-          child: RowBuilder(
-            itemCount: nOfColumns,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            itemBuilder: (context, indexOfColumn) {
-              return Expanded(
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  children: List.generate(
-                      lengthOfColumnList(itemCount, nOfColumns, indexOfColumn),
-                      (columnIndex) => this.itemBuilder(
-                          context,
-                          trueIndex(indexOfColumn, columnIndex,
-                              nOfColumns))).toList(),
-                ),
-              );
-            },
+    return RowBuilder(
+      mainAxisSize: MainAxisSize.max,
+      itemCount: widget.nOfColumns,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      itemBuilder: (context, indexOfColumn) {
+        return Expanded(
+          child: ListView(
+            controller: controllers[indexOfColumn],
+            shrinkWrap: widget.shrinkWrap,
+            padding: EdgeInsets.zero,
+            scrollDirection: Axis.vertical,
+            physics: widget.physics,
+            children: List.generate(
+                    lengthOfColumnList(
+                        widget.itemCount, widget.nOfColumns, indexOfColumn),
+                    (columnIndex) => this.widget.itemBuilder(
+                        context,
+                        trueIndex(
+                            indexOfColumn, columnIndex, widget.nOfColumns)))
+                .toList(),
           ),
-        ));
+        );
+      },
+    );
   }
 }
 
-// length of column list
+/// length of column list
 int lengthOfColumnList(int listLength, int nOfColumns, int columnIndex) {
   int r = listLength % nOfColumns; // remainder
   int baseLength = ((listLength - r) / nOfColumns).round();
   return baseLength + ((columnIndex + 1 <= r) ? 1 : 0);
 }
 
-// convert index of column and columnIndex to true index
+/// convert index of column and columnIndex to true index
 int trueIndex(indexOfColumn, columnIndex, nOfCOlumns) =>
     (indexOfColumn + 1) + nOfCOlumns * columnIndex - 1;
